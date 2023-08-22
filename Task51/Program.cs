4b82a049af2217d6d21318c5764d22081c4716f7
Елена <lenna@gmail.com>
2023-08-22 20:57:09 +0300

    Added program in Task51

--- Task51/Program.cs
+++ Task51/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] CreateMartixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("| ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],+5}");
+
+        }
+        Console.WriteLine("| ");
+    }
+
+}
+int SumElemDig(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+                sum += matrix[i, j];
+
+        }
+    }
+    return sum;
+}
+
+int[,] array_2d = CreateMartixRndInt(3, 3, 1, 5);
+PrintMatrix(array_2d);
+int sum_elem = SumElemDig(array_2d);
+Console.WriteLine($"Сумма элементов по диагонали = {sum_elem}");
